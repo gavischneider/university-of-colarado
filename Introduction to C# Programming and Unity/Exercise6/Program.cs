08699c198b5aba4c4e7d770eee41d8964949573d
Gavi Schneider <gavischneider@gmail.com>
2020-11-22 16:08:46 +0200

    Added exercise6

---
+++ Introduction to C# Programming and Unity/Exercise6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an angle in degress: ");
+            int angle = int.Parse(Console.ReadLine());
+            double angleInRadians = angle * (float) Math.PI / 180 ;
+            double sinAngle = Math.Sin(angleInRadians);
+            double cosAngle = Math.Cos(angleInRadians);
+            Console.WriteLine("Sin: " + sinAngle);
+            Console.WriteLine("Cos: " + cosAngle);
+        }
+    }
+}
