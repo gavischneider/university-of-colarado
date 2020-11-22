50b5d58b09cba30d179250513ab2136c1121762d
Gavi Schneider <gavischneider@gmail.com>
2020-11-22 12:57:39 +0200

    Added exercise4

---
+++ Introduction to C# Programming and Unity/Exercise4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first altitude: ");
+            int firstAltitude = int.Parse(Console.ReadLine());
+            Console.Write("Enter second altitude: ");
+            int secondAltitude = int.Parse(Console.ReadLine());
+            int difference = firstAltitude - secondAltitude;
+            Console.Write("The difference is: " + difference);
+        }
+    }
+}
