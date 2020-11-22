68b04624c8451c4ae9039c716c4f55886fa5a657
Gavi Schneider <gavischneider@gmail.com>
2020-11-22 12:51:17 +0200

    Added exercise3

---
+++ Introduction to C# Programming and Unity/Exercise3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 27;
+            Console.WriteLine("Age: " + age);
+        }
+    }
+}
