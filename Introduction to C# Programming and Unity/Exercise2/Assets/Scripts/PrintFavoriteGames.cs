201cfc5af708d7edca7c94c58c1e248d142ae020
Gavi Schneider <gavischneider@gmail.com>
2020-11-22 12:00:42 +0200

    Added exercise2

---
+++ Introduction to C# Programming and Unity/Exercise2/Assets/Scripts/PrintFavoriteGames.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintFavoriteGames : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("GTA, Age of Empires and Mario");
+    }
+
+}
